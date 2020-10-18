0c4b8c595d25c7ba9d73cc7ef42877d9a85fd9a6
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-10-18 18:51:02 -0400

    Random number generator function

--- Monte-Carlo Simulation.py
+++ Monte-Carlo Simulation.py
@@ -1,2 +1,39 @@
 # This project is made for Emmanuel Ogunjirin (eao5xc) and Kieran Humphrey (kdh8az)
 # This is for our Probability project.
+
+# Imports needed for the projects.
+import math
+
+# Defines variables to be used in the project
+k = pow(2, 14)
+customer_limit = 500
+
+# Defines variables that are changing
+x = 1000
+
+
+def rannumgen():
+    """
+    Function to provide information for the random number
+        generator using the linear congruential random number
+        generator rule
+
+    Returns:
+        [int]: [value of the linear congruential random number]
+    """
+    global x  # Accesses outside variable
+
+    # Set some constants
+    a = 9429
+    c = 3967
+
+    # Formula defines in appendix of project
+    x_i = ((a * x) + c) % k
+    x = x_i  # Reassigns the previous value
+    u_i = x_i / k
+    return u_i
+
+
+# Iterate of
+for i in range(3):
+    print(rannumgen())
