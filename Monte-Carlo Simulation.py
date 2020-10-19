f7dcee67c1b1efe1ff8587a0928f40b383668442
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-10-18 23:04:03 -0400

    Simulation for part 2

--- Monte-Carlo Simulation.py
+++ Monte-Carlo Simulation.py
@@ -9,7 +9,7 @@ BUSY_PROBABILITY = 0.2
 UNAVAILABLE_PROBABILITY = 0.3
 AVAILABLE_PROBABILITY = 0.5
 k = pow(2, 14)
-customer_limit = 6
+customer_limit = 500
 
 BUSY_STRING = "BUSY"
 UNAVAILABLE_STRING = "UNAVAILABLE"
@@ -34,37 +34,50 @@ def rannumgen():
     a = 9429
     c = 3967
 
-    # Formula defines in appendix of project
-    x_i = ((a * x) + c) % k
+    x_i = ((a * x) + c) % k  # Formula defines in appendix of project
     x = x_i  # Reassigns the previous value
-    u_i = x_i / k
+    u_i = x_i / k   # Formula defined in the appendix
     return u_i
+
+# For question 2A uncomment this section to run it.
+# for i in range(customer_limit):     # Iterates over the span of customers
+#     if(i >= 51 and i <= 53):   # Checks for the range we need
+#         print("Value for i =", i, "is", rannumgen())        # Prints the values
 
 
 # Iterates over the specific range needed
 for i in range(customer_limit):
-    RECALL_LIMIT = 4  # This is the maximum redial possible
-    seconds = 0    # Initial time to start calling someone
-    randomnumber = rannumgen()  # Runs the random number generator
+    RECALL_LIMIT = 5  # This is the maximum redial possible
+    seconds = 0  # Initial time to start calling someone
 
-    for j in range(0, RECALL_LIMIT, 1):
-        seconds += 6     # Initial time to pick up phone and dial a number
+    print("<->")  # Separates individuals calls from each other
 
-        if (j <= RECALL_LIMIT):  # Checks if we are still within redial conditions
-            if (randomnumber <= BUSY_PROBABILITY):
-                seconds += 4    # Adds time to realize person is busy
-            elif (randomnumber > BUSY_PROBABILITY and randomnumber <= AVAILABLE_PROBABILITY):
-                seconds += 26   # Adds time to realize person is unavailable
-            else:
-                seconds += 0
-                break
-        else:   # If we have passed maximum number of redials
-            print("Did not answer")
+    for j in range(0, RECALL_LIMIT, 1):     # Redials the user as many times as needed
+        randomnumber = rannumgen()  # Runs the random number generator
+        seconds += 6  # Initial time to pick up phone and dial a number
 
-    print("Probability", randomnumber, "gives", seconds, "seconds")
+        if (randomnumber <= BUSY_PROBABILITY):  # Busy probability
+            seconds += 4  # Adds time to realize person is busy
+            if(j != 4):  # Checks if we are not on the last try
+                print("Probability", randomnumber,
+                      "shows user is busy. Redialing...")
+        elif (randomnumber > BUSY_PROBABILITY and randomnumber <= AVAILABLE_PROBABILITY):  # Unavailalble section
+            seconds += 26  # Adds time to realize person is unavailable
+            if(j != 4):  # Checks if we are not on the last try
+                print("Probability", randomnumber,
+                      "shows user is unavailable. Redialing...")
+        else:   # Available section
+            # Find the time using the inverse function
+            function = -12 * math.log(1 - randomnumber)
 
+            if (function >= 25):    # Checks if time is over what is needed
+                seconds += 26   # Adds in the maximum seconds
+            else:   # If time is less than what is needed
+                seconds += function  # This will be x seconds
+                print("Probability", randomnumber, "gives", "function",
+                      function, "showing user picked up in", seconds, "seconds")
+                break   # Exits the loop for this user
 
-# # For question 2A
-# for i in range(customer_limit):     # Iterates over the span of customers
-#     if(i >= 51 and i <= 53):   # Checks for the range we need
-#         print("Value for i =", i, "is", rannumgen())        # Prints the values
+        if(j == 4):  # Checks if we are on the last try
+            print("Probability", randomnumber, "gives", "function",
+                  function, "showing user did NOT pick up in given time. User picked up in", seconds, "seconds")
