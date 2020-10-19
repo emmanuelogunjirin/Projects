258d5e7e2580ddfd88bee9dbd90a5ca3ba8db3af
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-10-18 20:42:20 -0400

    Part 2b solidified

--- Monte-Carlo Simulation.py
+++ Monte-Carlo Simulation.py
@@ -5,8 +5,15 @@
 import math
 
 # Defines variables to be used in the project
+BUSY_PROBABILITY = 0.2
+UNAVAILABLE_PROBABILITY = 0.3
+AVAILABLE_PROBABILITY = 0.5
 k = pow(2, 14)
-customer_limit = 500
+customer_limit = 6
+
+BUSY_STRING = "BUSY"
+UNAVAILABLE_STRING = "UNAVAILABLE"
+AVAILABLE_STRING = "AVAILABLE"
 
 # Defines variables that are changing
 x = 1000
@@ -19,7 +26,7 @@ def rannumgen():
         generator rule
 
     Returns:
-        [int]: [value of the linear congruential random number]
+        [decimal]: [value of the linear congruential random number]
     """
     global x  # Accesses outside variable
 
@@ -34,6 +41,30 @@ def rannumgen():
     return u_i
 
 
-# Iterate of
-for i in range(3):
-    print(rannumgen())
+# Iterates over the specific range needed
+for i in range(customer_limit):
+    RECALL_LIMIT = 4  # This is the maximum redial possible
+    seconds = 0    # Initial time to start calling someone
+    randomnumber = rannumgen()  # Runs the random number generator
+
+    for j in range(0, RECALL_LIMIT, 1):
+        seconds += 6     # Initial time to pick up phone and dial a number
+
+        if (j <= RECALL_LIMIT):  # Checks if we are still within redial conditions
+            if (randomnumber <= BUSY_PROBABILITY):
+                seconds += 4    # Adds time to realize person is busy
+            elif (randomnumber > BUSY_PROBABILITY and randomnumber <= AVAILABLE_PROBABILITY):
+                seconds += 26   # Adds time to realize person is unavailable
+            else:
+                seconds += 0
+                break
+        else:   # If we have passed maximum number of redials
+            print("Did not answer")
+
+    print("Probability", randomnumber, "gives", seconds, "seconds")
+
+
+# # For question 2A
+# for i in range(customer_limit):     # Iterates over the span of customers
+#     if(i >= 51 and i <= 53):   # Checks for the range we need
+#         print("Value for i =", i, "is", rannumgen())        # Prints the values
